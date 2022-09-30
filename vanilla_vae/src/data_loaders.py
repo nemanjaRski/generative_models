54851f7f2b43b412c6aa1a3206c2e9b670fe528a
nrasajski <nemanja.rasajski@gmail.com>
2022-09-30 19:19:52 +0200

    Added new data loaders

--- vanilla_vae/src/data_loaders.py
+++ vanilla_vae/src/data_loaders.py
@@ -1,6 +1,8 @@
-from torch.utils.data import DataLoader, random_split
-from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split, Dataset
+from torchvision.datasets import MNIST, LFWPeople
 from typing import Optional
+import pandas as pd
+from torchvision.io import read_image
 import pytorch_lightning as pl
 import os
 from torchvision import transforms
@@ -10,15 +12,15 @@ class MNISTDataModule(pl.LightningDataModule):
     def __init__(self, batch_size=64):
         super().__init__()
         self.batch_size = batch_size
+        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
 
     def prepare_data(self):
         # download only
-        MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor())
+        MNIST(os.getcwd(), train=True, download=True, transform=self.transform)
 
     def setup(self, stage: Optional[str] = None):
         # transform
-        transform = transforms.Compose([transforms.ToTensor()])
-        mnist_train = MNIST(os.getcwd(), train=True, download=False, transform=transform)
+        mnist_train = MNIST(os.getcwd(), train=True, download=False, transform=self.transform)
 
         # train/val split
         mnist_train, mnist_val = random_split(mnist_train, [55000, 5000])
@@ -32,3 +34,105 @@ class MNISTDataModule(pl.LightningDataModule):
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size)
+
+
+class LFWPeopleDataModule(pl.LightningDataModule):
+    def __init__(self, batch_size=32):
+        super().__init__()
+        self.batch_size = batch_size
+        self.transform = transforms.Compose([transforms.Resize((64, 64)), transforms.ToTensor()])
+        # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+
+    def prepare_data(self):
+        # download only
+        LFWPeople(os.getcwd(), split='train', download=True, transform=self.transform)
+        LFWPeople(os.getcwd(), split='test', download=True, transform=self.transform)
+
+    def setup(self, stage: Optional[str] = None):
+        # transform
+        lfw_train = LFWPeople(os.getcwd(), split='train', download=False, transform=self.transform)
+        lfw_val = LFWPeople(os.getcwd(), split='test', download=False, transform=self.transform)
+
+        # assign to use in dataloaders
+        self.train_dataset = lfw_train
+        self.val_dataset = lfw_val
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+
+
+class CelebADataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+
+class CropCelebA64(object):
+    """ This class applies cropping for CelebA64. This is a simplified implementation of:
+    https://github.com/andersbll/autoencoding_beyond_pixels/blob/master/dataset/celeba.py
+    """
+    def __call__(self, pic):
+        new_pic = pic.crop((15, 40, 178 - 15, 218 - 30))
+        return new_pic
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
+
+
+class CelebADataModule(pl.LightningDataModule):
+    def __init__(self, batch_size=64):
+        super().__init__()
+        self.batch_size = batch_size
+        self.transform = transforms.Compose([
+            transforms.ToPILImage(),
+            CropCelebA64(),
+            transforms.Resize((64, 64)),
+            transforms.ToTensor()])
+        # self.transform = transforms.Compose([
+        #     transforms.ToPILImage(),
+        #     transforms.ToTensor()])
+
+    def prepare_data(self):
+        # download only
+        pass
+
+    def setup(self, stage: Optional[str] = None):
+        # transform
+        # celebA_dataset = CelebADataset(annotations_file='E:/Datasets/CelebA64/list_attr_celeba.csv',
+        #                                img_dir='E:/Datasets/CelebA64/img_align_celeba/img_align_celeba',
+        #                                transform=self.transform)
+        celebA_dataset = CelebADataset(annotations_file='E:/Datasets/CelebA/list_attr_celeba.csv',
+                                       img_dir='E:/Datasets/CelebA/img_align_celeba/img_align_celeba',
+                                       transform=self.transform)
+
+        celebA_train, celebA_val = random_split(celebA_dataset, [162079, 40520])
+
+        # assign to use in dataloaders
+        self.train_dataset = celebA_train
+        self.val_dataset = celebA_val
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+
+
